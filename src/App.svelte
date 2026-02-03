a26a8370de15dc1c41a0af5d13d7eed0dbd1130f
Daniel Sýkora <git@silver-volt4.cz>
2026-02-03 14:20:46 +0100

    feat: add fullscreen button

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
     import BinaryHeapViewer from "./lib/binary_heap/BinaryHeapViewer.svelte";
+
+    let fullscreenElement: Element | undefined = $state();
+
+    function fullscreen() {
+        if (fullscreenElement) {
+            document.exitFullscreen();
+        } else {
+            document.body.requestFullscreen();
+        }
+    }
 </script>
+
+<svelte:document bind:fullscreenElement />
 
 <div
     class="h-full w-full flex flex-col gap-2 p-2 box-border dark:text-white text-black dark:bg-slate-900 bg-slate-300"
 >
-    <BinaryHeapViewer/>
+    <div
+        class="dark:bg-slate-950 bg-slate-200 rounded-lg overflow-hidden p-2 flex"
+    >
+        <div class="grow">AG1 Trainer</div>
+        <div>
+            <button onclick={fullscreen}>Full screen</button>
+        </div>
+    </div>
+    <BinaryHeapViewer />
 </div>
