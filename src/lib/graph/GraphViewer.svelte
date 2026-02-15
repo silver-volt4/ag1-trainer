755bd608a5777fcfcc102984957b9d0cd4d980e1
Daniel Sýkora <git@silver-volt4.cz>
2026-02-15 11:09:31 +0100

    feat: rework controls into attachments

--- src/lib/graph/GraphViewer.svelte
+++ src/lib/graph/GraphViewer.svelte
@@ -1,134 +1,28 @@
 <script lang="ts">
   import { BaseGraph, type IEdge, type IVertex } from "./graph.svelte";
+  import {
+    mouseViewportController,
+    touchViewportController,
+    ViewportController,
+  } from "./graph_controls.svelte";
   import Vertex from "./Vertex.svelte";
 
-  let {
-    graph,
-  }: {
-    graph: BaseGraph<IVertex, IEdge>;
-  } = $props();
+  let { graph }: { graph: BaseGraph<IVertex, IEdge> } = $props();
 
-  function clamp(min: number, value: number, max: number) {
-    return Math.min(max, Math.max(value, min));
-  }
-
-  let viewportWidth = $state(0);
-  let viewportHeight = $state(0);
-
-  let centerX = $state(0);
-  let centerY = $state(0);
-  let zoom = $state(3.0);
-
-  // SVG ViewBox calculation
-  let viewBoxMinX = $derived(centerX - viewportWidth / (2 * zoom));
-  let viewBoxMinY = $derived(centerY - viewportHeight / (2 * zoom));
-  let viewBoxWidth = $derived(viewportWidth / zoom);
-  let viewBoxHeight = $derived(viewportHeight / zoom);
-  let viewBox = $derived(
-    `${viewBoxMinX} ${viewBoxMinY} ${viewBoxWidth} ${viewBoxHeight}`,
-  );
-
-  function onwheel(event: WheelEvent) {
-    let factor = event.deltaY / -200;
-
-    let xWithinSvgBefore =
-      viewBoxMinX + (event.offsetX / viewportWidth) * viewBoxWidth;
-    let yWithinSvgBefore =
-      viewBoxMinY + (event.offsetY / viewportHeight) * viewBoxHeight;
-
-    zoom = clamp(1, (zoom += factor), 20);
-
-    let xWithinSvgAfter =
-      viewBoxMinX + (event.offsetX / viewportWidth) * viewBoxWidth;
-    let yWithinSvgAfter =
-      viewBoxMinY + (event.offsetY / viewportHeight) * viewBoxHeight;
-
-    centerX += xWithinSvgBefore - xWithinSvgAfter;
-    centerY += yWithinSvgBefore - yWithinSvgAfter;
-  }
-
-  function onmousedown(event: MouseEvent) {
-    const AUXILIARY_BUTTON_ID = 1; // this refers to the middle mouse button
-    if (event.button === AUXILIARY_BUTTON_ID) {
-      console.info("Start dragging motion");
-
-      window.addEventListener("mousemove", mousemove);
-
-      const detach = (e: MouseEvent) => {
-        if (e.button === AUXILIARY_BUTTON_ID) {
-          console.info("End dragging motion");
-          window.removeEventListener("mousemove", mousemove);
-          window.removeEventListener("mouseup", detach);
-        }
-      };
-
-      window.addEventListener("mouseup", detach);
-    }
-  }
-
-  let touchDelta: { x: number; y: number } | null = null;
-  let dist: number | null = null;
-
-  function ontouchstart(event: TouchEvent) {
-    event.preventDefault();
-    if (event.touches.length === 1) {
-      touchDelta = {
-        x: event.touches[0].clientX,
-        y: event.touches[0].clientY,
-      };
-      dist = null;
-    }
-    window.addEventListener("touchmove", ontouchmove, { passive: false });
-    window.addEventListener(
-      "touchend",
-      () => {
-        window.removeEventListener("touchmove", ontouchmove);
-      },
-      { once: true },
-    );
-  }
-
-  function ontouchmove(event: TouchEvent) {
-    event.preventDefault();
-    if (event.touches.length === 1) {
-      if (touchDelta) {
-        centerX -= (event.touches[0].clientX - touchDelta.x) / zoom;
-        centerY -= (event.touches[0].clientY - touchDelta.y) / zoom;
-      }
-      touchDelta = {
-        x: event.touches[0].clientX,
-        y: event.touches[0].clientY,
-      };
-    } else if (event.touches.length === 2) {
-      let newDist = Math.sqrt(
-        event.touches[0].clientX * event.touches[1].clientX +
-          event.touches[0].clientY * event.touches[1].clientY,
-      );
-      if (dist) {
-        zoom = Math.max(1.0, zoom + (dist - newDist) / 10);
-      }
-      dist = newDist;
-    }
-  }
-
-  function mousemove(event: MouseEvent) {
-    centerX -= event.movementX / zoom;
-    centerY -= event.movementY / zoom;
-  }
+  let viewportController = new ViewportController();
 </script>
 
 <div
   class="h-full w-full overflow-hidden relative"
-  bind:clientWidth={viewportWidth}
-  bind:clientHeight={viewportHeight}
-  {onwheel}
+  bind:clientWidth={viewportController.viewportWidth}
+  bind:clientHeight={viewportController.viewportHeight}
   oncontextmenu={(e: Event) => e.preventDefault()}
-  {onmousedown}
-  {ontouchstart}
+  {@attach mouseViewportController(viewportController)}
+  {@attach touchViewportController(viewportController)}
   role="presentation"
   aria-roledescription="Graph visualizer"
 >
-  <svg {viewBox} class="h-full w-full">
+  <svg viewBox={viewportController.viewBox} class="h-full w-full">
     {#each graph.getEdges() as edge (edge)}
       <path
         d="M{edge.from.x} {edge.from.y} L{edge.to.x} {edge.to.y}"
---
+++ src/lib/graph/graph_controls.svelte.ts
@@ -0,0 +1,147 @@
+function clamp(min: number, value: number, max: number) {
+    return Math.min(max, Math.max(value, min));
+}
+
+export class ViewportController {
+    x = $state(0);
+    y = $state(0);
+    zoom = $state(3.0);
+
+    viewportWidth = $state(0);
+    viewportHeight = $state(0);
+
+    private viewBoxMinX = $derived(this.x - this.viewportWidth / (2 * this.zoom));
+    private viewBoxMinY = $derived(this.y - this.viewportHeight / (2 * this.zoom));
+    private viewBoxWidth = $derived(this.viewportWidth / this.zoom);
+    private viewBoxHeight = $derived(this.viewportHeight / this.zoom);
+
+    viewBox = $derived(
+        `${this.viewBoxMinX} ${this.viewBoxMinY} ${this.viewBoxWidth} ${this.viewBoxHeight}`,
+    );
+
+    public pan(dx: number, dy: number): void {
+        this.x -= dx / this.zoom;
+        this.y -= dy / this.zoom;
+    }
+
+    public zoomAtViewportPosition(x: number, y: number, dz: number) {
+        let ratX = x / this.viewportWidth;
+        let ratY = y / this.viewportHeight;
+
+        let xWithinSvgBefore = this.viewBoxMinX + ratX * this.viewBoxWidth;
+        let yWithinSvgBefore = this.viewBoxMinY + ratY * this.viewBoxHeight;
+
+        this.zoom = clamp(1, (this.zoom += dz), 20);
+
+        let xWithinSvgAfter = this.viewBoxMinX + ratX * this.viewBoxWidth;
+        let yWithinSvgAfter = this.viewBoxMinY + ratY * this.viewBoxHeight;
+
+        this.x += xWithinSvgBefore - xWithinSvgAfter;
+        this.y += yWithinSvgBefore - yWithinSvgAfter;
+    }
+}
+
+export function mouseViewportController(viewportController: ViewportController) {
+    return function (node: HTMLElement) {
+        const MIDDLE_MOUSE_BUTTON_ID = 1;
+
+        node.addEventListener("mousedown", onmousedown)
+        node.addEventListener("mouseup", onmouseup)
+        node.addEventListener("wheel", onwheel)
+
+        function onmousedown(event: MouseEvent) {
+            if (event.button === MIDDLE_MOUSE_BUTTON_ID) {
+                console.info("Start dragging motion");
+                window.addEventListener("mousemove", onmousemove);
+            }
+        }
+
+        function onmouseup(e: MouseEvent) {
+            if (e.button === MIDDLE_MOUSE_BUTTON_ID) {
+                console.info("End dragging motion");
+                window.removeEventListener("mousemove", onmousemove);
+            }
+        };
+
+        function onmousemove(event: MouseEvent) {
+            viewportController.pan(event.movementX, event.movementY);
+        }
+
+        function onwheel(event: WheelEvent) {
+            let factor = -Math.sign(event.deltaY);
+            viewportController.zoomAtViewportPosition(
+                event.offsetX,
+                event.offsetY,
+                factor,
+            );
+        }
+
+        return () => {
+            node.removeEventListener("mousedown", onmousedown);
+            node.removeEventListener("mouseup", onmouseup);
+            node.removeEventListener("wheel", onwheel);
+            window.removeEventListener("mousemove", onmousemove);
+        }
+    }
+}
+
+
+export function touchViewportController(viewportController: ViewportController) {
+    return function (node: HTMLElement) {
+        let oldTouches: { [key: number]: Touch } = {};
+
+        node.addEventListener("touchstart", ontouchstart, { passive: false });
+        node.addEventListener("touchmove", ontouchmove, { passive: false });
+        node.addEventListener("touchend", ontouchend, { passive: false });
+
+        function ontouchstart(event: TouchEvent) {
+            for (let touch of event.touches) {
+                oldTouches[touch.identifier] = touch;
+            }
+        }
+
+        function ontouchend(event: TouchEvent) {
+            for (let touch of event.touches) {
+                delete oldTouches[touch.identifier];
+            }
+        }
+
+        function ontouchmove(event: TouchEvent) {
+            if (event.touches.length === 1) {
+                let current = event.touches[0];
+                let previous = oldTouches[current.identifier] ?? null;
+                if (previous) {
+                    viewportController.pan(current.clientX - previous.clientX, current.clientY - previous.clientY);
+                }
+            } else if (event.touches.length === 2) {
+                let t0 = event.touches[0];
+                let t1 = event.touches[1];
+                let pt0 = oldTouches[t0.identifier] ?? null;
+                let pt1 = oldTouches[t1.identifier] ?? null;
+                if (pt0 && pt1) {
+                    let distBefore = Math.sqrt(t0.clientX * t1.clientX + t0.clientY * t1.clientY);
+                    let distAfter = Math.sqrt(pt0.clientX * pt1.clientX + pt0.clientY * pt1.clientY);
+
+                    let dz = 0;
+                    if (distBefore != 0) {
+                        dz = distAfter / distBefore - 1;
+                    }
+
+                    dz *= 100;
+
+                    viewportController.zoomAtViewportPosition((t0.clientX + t1.clientX) / 2, (t0.clientY + t1.clientY) / 2, dz);
+                }
+            }
+
+            for (let touch of event.touches) {
+                oldTouches[touch.identifier] = touch;
+            }
+        }
+
+        return () => {
+            node.removeEventListener("touchstart", ontouchstart);
+            node.removeEventListener("touchmove", ontouchmove);
+            node.removeEventListener("touchend", ontouchend);
+        }
+    }
+}
